12a8f391de673e7520eaff6ceab98ad1efb384ae
Minoo <minoo.hadi2904@gmail.com>
2022-10-21 20:47:33 +0330

    create addTodo function

--- script.js
+++ script.js
@@ -37,3 +37,65 @@ function loginSubmit(event) {
   }
 }
 
+let todo = document.querySelector("ul");
+function addTodo(event) {
+  event.preventDefault();
+  let liList = {
+    work: document.getElementById("work").value,
+    time: document.getElementById("time").value,
+  };
+  let liListString = JSON.stringify(liList);
+  localStorage.setItem("lilist", liListString);
+
+  let getItem = localStorage.getItem("lilist");
+  getItem = JSON.parse(getItem);
+  console.log(getItem);
+  let item = document.createElement("li");
+  
+  let getWork = document.createElement("p");
+  getWork.innerText = getItem["work"];
+  item.prepend(getWork);
+  let getTime = document.createElement("div");
+  getTime.innerText = getItem["time"];
+  item.append(getTime);
+  let getCheckBox = document.createElement("input");
+  getCheckBox.type = "checkBox";
+  getCheckBox.id = Math.floor(Math.random() * 1000);
+  item.prepend(getCheckBox);
+
+  let deletIcone=document.createElement("div")
+  deletIcone.id=Math.floor(Math.random() * 1000);
+  deletIcone.innerText="D"
+  deletIcone.style.backgroundColor="green";
+  deletIcone.style.marginRight="50px";
+  deletIcone.style.width="30px";
+  deletIcone.style.height="30px";
+
+  item.append(deletIcone);
+  todo.append(item);
+
+  let selected = getCheckBox.addEventListener("click", (event) => {
+    checkTodo(getCheckBox.id);
+  });
+  function checkTodo(id) {
+    console.log(id);
+    if(getCheckBox.checked){
+      getWork.classList.add("checked");
+      item.classList.add("back")
+    }
+    else{
+      getWork.classList.remove("checked");
+      item.classList.remove("back")
+    }
+  }
+  let selectDelete=deletIcone.addEventListener("click" , (event) => {
+    delet(deletIcone.id);
+  })
+function delet(id){
+  console.log(id);
+  localStorage.removeItem("liList");
+
+}
+
+}
+
