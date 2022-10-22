33aa0ce1ecaa11ef0877f83ade46b04fa47c423b
Minoo <minoo.hadi2904@gmail.com>
2022-10-22 14:48:10 +0330

    final edit

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-let day = document.getElementsByClassName("day");
-// console.log(day);
+let days = document.getElementsByClassName("day");
+let todo = document.querySelector("ul");
 
-function deletBorder(elm) {
-  // console.log(elm);
-  for (let i = 0; i < day.length; i++) {
-    day[i].classList.remove("active-div");
-  }
-  elm.classList.add("active-div");
+let loggedIn = localStorage.getItem("login-group1");
+if (loggedIn) {
+  let loginPopup = document.getElementsByClassName("login")[0];
+  loginPopup.style.display = "none";
+} else {
+  document.getElementById("add-btn").disabled = true;
 }
 
-// let addButton = document.getElementById("add-btn");
-// function addTodo(){
+let selectedDay;
+let globalArray = localStorage.getItem("globalArray");
+if (!globalArray) {
+  globalArray = [
+    { day: "shanbe", task: [] },
+    { day: "yekshanbe", task: [] },
+    { day: "doshanbe", task: [] },
+    { day: "seshanbe", task: [] },
+    { day: "chaharshanbe", task: [] },
+    { day: "panjshanbe", task: [] },
+    { day: "jome", task: [] },
+  ];
+} else {
+  globalArray = JSON.parse(globalArray);
+}
 
-// }
+function save() {
+  localStorage.setItem("globalArray", JSON.stringify(globalArray));
+}
+
+function generateId() {
+  return Date.now();
+}
+
+function deletBorder(elm) {
+  for (let i = 0; i < days.length; i++) {
+    days[i].classList.remove("active-div");
+  }
+  elm.classList.add("active-div");
+  selectedDay = Number(elm.attributes["data-index"].value);
+  todo.innerHTML = "";
+  for (let data of globalArray[selectedDay].task) {
+    renderData(data);
+  }
+}
 
 function loginSubmit(event) {
   event.preventDefault();
@@ -33,69 +64,113 @@ function loginSubmit(event) {
     error.style.display = "block";
   } else {
     let loginString = JSON.stringify(login);
-    localStorage.setItem("login", loginString);
+    localStorage.setItem("login-group1", loginString);
+    let loginPopup = document.getElementsByClassName("login")[0];
+    loginPopup.style.display = "none";
+    document.getElementById("add-btn").disabled = false;
   }
 }
 
-let todo = document.querySelector("ul");
 function addTodo(event) {
+  if (selectedDay === undefined) {
+    return;
+  }
   event.preventDefault();
-  let liList = {
-    work: document.getElementById("work").value,
-    time: document.getElementById("time").value,
+  let liItem = {
+    id: generateId(),
+    taskName: document.getElementById("taskName").value,
+    taskTime: document.getElementById("taskTime").value,
+    isDone: false,
   };
-  let liListString = JSON.stringify(liList);
-  localStorage.setItem("lilist", liListString);
+  globalArray[selectedDay]["task"].push(liItem);
+  save();
+  document.getElementById("taskName").value = "";
+  document.getElementById("taskTime").value = "";
 
-  let getItem = localStorage.getItem("lilist");
-  getItem = JSON.parse(getItem);
-  console.log(getItem);
+  renderData(liItem);
+}
+
+function renderData(data) {
   let item = document.createElement("li");
-  
-  let getWork = document.createElement("p");
-  getWork.innerText = getItem["work"];
-  item.prepend(getWork);
-  let getTime = document.createElement("div");
-  getTime.innerText = getItem["time"];
-  item.append(getTime);
-  let getCheckBox = document.createElement("input");
-  getCheckBox.type = "checkBox";
-  getCheckBox.id = Math.floor(Math.random() * 1000);
-  item.prepend(getCheckBox);
+  item.id = data.id;
 
-  let deletIcone=document.createElement("div")
-  deletIcone.id=Math.floor(Math.random() * 1000);
-  deletIcone.innerText="D"
-  deletIcone.style.backgroundColor="green";
-  deletIcone.style.marginRight="50px";
-  deletIcone.style.width="30px";
-  deletIcone.style.height="30px";
+  let taskName = document.createElement("p");
+  taskName.innerText = data.taskName;
+  if (data.isDone) {
+    taskName.classList.add("checked");
+    item.classList.add("back");
+  }
+  item.prepend(taskName);
 
+  let taskTime = document.createElement("div");
+  taskTime.innerText = data.taskTime;
+  item.append(taskTime);
+
+  let checkBox = document.createElement("input");
+  checkBox.type = "checkBox";
+  checkBox.checked = data.isDone;
+  item.prepend(checkBox);
+
+  let deletIcone = document.createElement("div");
+  deletIcone.innerText = "D";
+  deletIcone.style.backgroundColor = "red";
+  deletIcone.style.marginRight = "50px";
+  deletIcone.style.width = "30px";
+  deletIcone.style.height = "30px";
   item.append(deletIcone);
+
+  let editIcon = document.createElement("div");
+  editIcon.innerText = "E";
+  editIcon.style.backgroundColor = "green";
+  editIcon.style.marginRight = "60px";
+  editIcon.style.width = "30px";
+  editIcon.style.height = "30px";
+  item.append(editIcon);
+
   todo.append(item);
 
-  let selected = getCheckBox.addEventListener("click", (event) => {
-    checkTodo(getCheckBox.id);
+  let selected = checkBox.addEventListener("click", (event) => {
+    data.isDone = !data.isDone;
+    save();
+    if (checkBox.checked) {
+      taskName.classList.add("checked");
+      item.classList.add("back");
+    } else {
+      taskName.classList.remove("checked");
+      item.classList.remove("back");
+    }
   });
-  function checkTodo(id) {
-    console.log(id);
-    if(getCheckBox.checked){
-      getWork.classList.add("checked");
-      item.classList.add("back")
-    }
-    else{
-      getWork.classList.remove("checked");
-      item.classList.remove("back")
-    }
-  }
-  let selectDelete=deletIcone.addEventListener("click" , (event) => {
-    delet(deletIcone.id);
-  })
-function delet(id){
-  console.log(id);
-  localStorage.removeItem("liList");
 
+  let selectDelete = deletIcone.addEventListener("click", (event) => {
+    let confirmed = confirm("آیا میخواهید این آیتم حذف شود؟");
+    if (confirmed) {
+      globalArray[selectedDay].task = globalArray[selectedDay].task.filter(
+        (item) => item.id !== data.id
+      );
+      save();
+      todo.removeChild(item);
+    }
+  });
+
+  let selectEdit = editIcon.addEventListener("click", (event) => {
+    let editBox = document.getElementsByClassName("editBox")[0];
+    editBox.style.visibility = "visible";
+    let editText = document.getElementById("editText");
+    editText.value = data.taskName;
+    editBox.setAttribute("data-id", data.id);
+  });
 }
 
-}
-
+let subBtn = document.getElementById("sub-btn");
+let subEdit = subBtn.addEventListener("click", (event) => {
+  let editBox = document.getElementsByClassName("editBox")[0];
+  let itemId = editBox.getAttribute("data-id");
+  let li = document.getElementById(itemId);
+  let p = li.getElementsByTagName("p")[0];
+  p.innerText = document.getElementById("editText").value;
+  editBox.style.visibility = "hidden";
+  let array = globalArray[selectedDay].task;
+  let result = array.find(arr => arr.id == itemId);
+  result.taskName = p.innerText;
+  save();
+});
